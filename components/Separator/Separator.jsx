4ec28ffd42bd377524da129e1bea6c261b04cf9c
priscilla goessens <priscillagoessens@gmail.com>
2024-08-07 13:44:07 +0200

    convert img webp

--- components/Separator/Separator.jsx
+++ components/Separator/Separator.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import './_Separator.scss'
-import Image from 'next/image'
-import SeparatorImg from '@/public/assets/img/white-bd.jpg'
-
 
 export default function Separator() {
   return (
-    // <Image src={SeparatorImg} alt="banniere" className="line"/>
     <section className='separator-section'>
       <div className="separator-section-skewedLeft"></div>
     </section>
