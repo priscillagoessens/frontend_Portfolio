2eacece55f7478adb6d2b47a223e72fa0fe4c664
priscilla goessens <priscillagoessens@gmail.com>
2024-11-16 16:58:59 +0100

    ajout projet

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ export default function Banner() {
   return (
     <div className=' banner-container'>
       <div className=' banner-container-title'>
-        <h1 className=' banner-container-title-texte'>Bienvenue,<br/> je suis Priscilla, développeuse front end React </h1>
+        <h1 className=' banner-container-title-texte'>Développeuse Front-End :<br/> Donnez Vie à Vos Projets Web</h1>
       </div>
       <div className=" banner-container-scroll-downs">
         <div className=" banner-container-scroll-downs-mousey">
--- components/Details/Details.jsx
+++ components/Details/Details.jsx
@@ -10,13 +10,12 @@ export default function Details({sectionStyle, titleStyle, contentStyle, iconCon
           <h2 className='title'>A propos de moi</h2>
         </div>
         <p className={contentStyle}>
+        Je m'appelle Priscilla Goessens, développeuse front-end passionnée et basée dans la métropole lilloise. Mon objectif : créer des sites web qui combinent <strong>esthétisme</strong>, <strong>fonctionnalité</strong>, et <strong>expérience utilisateur optimale</strong>.<br/><br/>
 
-        Bonjour, je suis Priscilla Goessens, développeuse front-end basée dans la métropole lilloise. Je me passionne pour la création de sites web qui allient esthétisme et fonctionnalité. <br/>
-        Grâce à mes compétences en <strong>HTML</strong>, <strong>CSS</strong>, <strong>Sass</strong> et <strong>JavaScript</strong>, je conçois des interfaces qui sont à la fois attractives et intuitives.
+        Avec une expertise solide en <strong>HTML</strong>, <strong>CSS</strong>, <strong>Sass</strong>, et <strong>JavaScript</strong>, je conçois des interfaces intuitives et attrayantes. J'exploite des technologies modernes comme React.js et Next.js pour développer des applications web performantes, évolutives, et adaptées aux besoins actuels.<br/>
+        Mon expérience avec <strong></strong>WordPress et <strong></strong>WooCommerce m'a permis de réaliser des solutions personnalisées, optimisées pour des sites e-commerce réussis.<br/><br/>
 
-        J'utilise des technologies modernes comme <strong>React</strong>, <strong>Redux</strong>, <strong>Next.js</strong> et <strong>Tailwind</strong> pour développer des applications performantes et maintenables. J'ai également une solide expérience avec <strong>WordPress</strong> et <strong>WooCommerce</strong>, où j'ai pu personnaliser des solutions adaptées aux besoins de mes clients.
-        <br/>
-        Je suis actuellement à la recherche de nouvelles opportunités en tant que salariée indépendante. Si vous avez un projet en tête ou souhaitez discuter de vos idées, je serais ravie d'échanger avec vous. Ensemble, nous pouvons donner vie à vos projets web ! ✨
+        Je suis actuellement à la recherche de <strong>nouvelles opportunités</strong>, que ce soit en tant que salariée ou indépendante. Si vous avez un projet en tête ou souhaitez échanger autour de vos idées, je serais ravie de collaborer avec vous. <strong>Ensemble, donnons vie à vos projets web !</strong>
         </p>
         <div className={iconContainerStyle}>
         <Button link="Priscilla_Goessens_CV.pdf">
