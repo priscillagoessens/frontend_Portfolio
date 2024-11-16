edb8c58a20f63322aa06d06ed6561444f2842053
priscilla goessens <priscillagoessens@gmail.com>
2024-11-16 17:13:27 +0100

    ajout projet

--- components/Details/Details.jsx
+++ components/Details/Details.jsx
@@ -12,8 +12,8 @@ export default function Details({sectionStyle, titleStyle, contentStyle, iconCon
         <p className={contentStyle}>
         Je m'appelle Priscilla Goessens, développeuse front-end passionnée et basée dans la métropole lilloise. Mon objectif : créer des sites web qui combinent <strong>esthétisme</strong>, <strong>fonctionnalité</strong>, et <strong>expérience utilisateur optimale</strong>.<br/><br/>
 
-        Avec une expertise solide en <strong>HTML</strong>, <strong>CSS</strong>, <strong>Sass</strong>, et <strong>JavaScript</strong>, je conçois des interfaces intuitives et attrayantes. J'exploite des technologies modernes comme React.js et Next.js pour développer des applications web performantes, évolutives, et adaptées aux besoins actuels.<br/>
-        Mon expérience avec <strong></strong>WordPress et <strong></strong>WooCommerce m'a permis de réaliser des solutions personnalisées, optimisées pour des sites e-commerce réussis.<br/><br/>
+        Avec une expertise solide en <strong>HTML</strong>, <strong>CSS</strong>, <strong>Sass</strong>, et <strong>JavaScript</strong>, je conçois des interfaces intuitives et attrayantes. J'exploite des technologies modernes comme <strong>React js</strong> et <strong>Next.js</strong> pour développer des applications web performantes, évolutives, et adaptées aux besoins actuels.<br/>
+        Mon expérience avec <strong>WordPress</strong> et <strong>WooCommerce</strong> m'a permis de réaliser des solutions personnalisées, optimisées pour des sites e-commerce réussis.<br/><br/>
 
         Je suis actuellement à la recherche de <strong>nouvelles opportunités</strong>, que ce soit en tant que salariée ou indépendante. Si vous avez un projet en tête ou souhaitez échanger autour de vos idées, je serais ravie de collaborer avec vous. <strong>Ensemble, donnons vie à vos projets web !</strong>
         </p>
