20a873fe5462b7661acda7f4733633ce45850234
priscilla goessens <priscillagoessens@gmail.com>
2024-11-17 14:46:22 +0100

    maj cv

--- components/Details/Details.jsx
+++ components/Details/Details.jsx
@@ -18,8 +18,8 @@ export default function Details({sectionStyle, titleStyle, contentStyle, iconCon
         Je suis actuellement à la recherche de <strong>nouvelles opportunités</strong>, que ce soit en tant que salariée ou indépendante. Si vous avez un projet en tête ou souhaitez échanger autour de vos idées, je serais ravie de collaborer avec vous. <strong>Ensemble, donnons vie à vos projets web !</strong>
         </p>
         <div className={iconContainerStyle}>
-        <Button link="Priscilla_Goessens_CV.png">
-          Mon CV (pdf)
+        <Button link="Priscilla_Goessens_Cv_Developpeuse_Front.pdf">
+          Mon CV
         </Button>
           <Link href="https://github.com/priscillagoessens" target="_blank">
             <Image src={iconGit} alt="GitHub" />
